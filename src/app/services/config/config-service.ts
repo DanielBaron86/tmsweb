711c204e894119477da1f24ed8ce9457f8b4cb42
Daniel Baron <baron.daniel.86@gmail.com>
2026-02-23 23:58:14 +0200

    config

--- src/app/services/config/config-service.ts
+++ src/app/services/config/config-service.ts
@@ -11,27 +11,27 @@ export class ConfigService {
   readonly http = inject(HttpClient);
 
   private readonly DEFAULT_CONFIG = {
-    apiUrl: 'https://tmsapi.danielsplaygrounds.com/api',
+    apiUrl: 'https://localhost:7220/api',
     apStr: 'Default App String'
   };
 
   private config: any = this.DEFAULT_CONFIG;
 
-  // async loadConfig() {
-  //   if (isDevMode()) {
-  //     this.config = this.DEFAULT_CONFIG;
-  //     return;
-  //   }
-  //
-  //   try {
-  //     this.config = await lastValueFrom(
-  //       this.http.get('/assets/config.json').pipe(timeout(2000))
-  //     );
-  //   } catch (err) {
-  //     console.error('Could not load config, falling back to defaults', err);
-  //     this.config = this.DEFAULT_CONFIG;
-  //   }
-  // }
+  async loadConfig() {
+    if (isDevMode()) {
+      this.config = this.DEFAULT_CONFIG;
+      return;
+    }
+
+    try {
+      this.config = await lastValueFrom(
+        this.http.get('/assets/config.json').pipe(timeout(2000))
+      );
+    } catch (err) {
+      console.error('Could not load config, falling back to defaults', err);
+      this.config = this.DEFAULT_CONFIG;
+    }
+  }
 
   get apiUrl(): string {
     return this.config.apiUrl;
